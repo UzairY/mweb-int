08a71c13b3b83688fb66414c1e83c72539b9e09f
Uzair Yuseph <uzair.yuseph@sanlam.co.za>
2024-01-10 11:14:20 +0200

    added headers

--- src/login/main.py
+++ src/login/main.py
@@ -30,6 +30,11 @@ def lambda_handler(event, context):
         )
         return {
             'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*'
+            },
             'body': json.dumps(response)
         }
     except client.exceptions.NotAuthorizedException as e:
